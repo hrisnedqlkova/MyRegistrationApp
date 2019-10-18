9de1728ba8dc3eff68f2436e1fde00e929b29293
hrisnedqlkova <hrisnedqlkova@gmail.com>
2019-10-18 14:15:05 +0300

    Some code

--- RegistrationWeek3/RegistrationWeek3/Program.cs
+++ RegistrationWeek3/RegistrationWeek3/Program.cs
@@ -10,8 +10,44 @@ namespace RegistrationWeek3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Press 1 for registration or 2 for login");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    RegisterUser();
+                    break;
+
+                case "2":
+                    LoginUser();
+                    break;
+
+
+            }
+
+
+            Console.WriteLine("Username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Password");
+            string password = Console.ReadLine();
+            if (password.Length < 3)
+            {
+                Console.WriteLine("Password is too short ");
+            }
+
+            List<string> names = new List<string>()
+             {
+                "Hristiana Nedyalkova",
+               "Ivan Ivanov",
+               "Marina Stoilova",
+               "Krasimir Petkov"
+             };
+
+             foreach (var name in names)
+             {
+              Console.WriteLine(name);
+             }
+
         }
     }
 }
---
+++ RegistrationWeek3/RegistrationWeek3/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationWeek3
+{
+    public class User
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+
+}
